cdc62bb04ece6c6afbd9ff1200f11794c2a507d7
黄飞越 <lamisiamyou@163.com>
2019-08-12 19:58:53 +0800

    framebuffer，核模式

--- openGLTest/openGLTest/main.cpp
+++ openGLTest/openGLTest/main.cpp
@@ -64,16 +64,14 @@ int main()
         std::cout << "Failed to initialize GLAD" << std::endl;
         return -1;
     }
-    glEnable(GL_DEPTH_TEST);
-    glEnable(GL_BLEND);
-    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
-    // build and compile our shader zprogram
+    glEnable(GL_DEPTH_TEST);    // build and compile our shader zprogram
     // ------------------------------------
     //Shader ourShader("/Users/hfy/Svn/openGLTest/openGLTest/shader.vs", "/Users/hfy/Svn/openGLTest/openGLTest/shader.fs");
     //Model ourModel("/Users/hfy/Svn/openGLTest/openGLTest/nanosuit/nanosuit.obj");
     //Shader shaderSingleColor("/Users/hfy/Svn/openGLTest/openGLTest/shader.vs", "/Users/hfy/Svn/openGLTest/openGLTest/stencil_single_color.fs");
     
-    Shader shader("/Users/hfy/Svn/openGLTest/openGLTest/shader.vs","/Users/hfy/Svn/openGLTest/openGLTest/blend.fs");
+    Shader shader("/Users/hfy/Documents/GitHub/OpenGLTest/openGLTest/openGLTest/shader.vs","/Users/hfy/Documents/GitHub/OpenGLTest/openGLTest/openGLTest/blend.fs");
+    Shader screenShader("/Users/hfy/Documents/GitHub/OpenGLTest/openGLTest/openGLTest/framebuffer.vs", "/Users/hfy/Documents/GitHub/OpenGLTest/openGLTest/openGLTest/framebuffer.fs");
     glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
     glfwSetCursorPosCallback(window, mouse_callback);
     float transparentVertices[] = {
@@ -86,17 +84,17 @@ int main()
         1.0f, -0.5f,  0.0f,  1.0f,  1.0f,
         1.0f,  0.5f,  0.0f,  1.0f,  0.0f
     };
-    unsigned int transparentVAO, transparentVBO;
-    glGenVertexArrays(1, &transparentVAO);
-    glGenBuffers(1, &transparentVBO);
-    glBindVertexArray(transparentVAO);
-    glBindBuffer(GL_ARRAY_BUFFER, transparentVBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(transparentVertices), transparentVertices, GL_STATIC_DRAW);
-    glEnableVertexAttribArray(0);
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
-    glEnableVertexAttribArray(1);
-    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
-    glBindVertexArray(0);
+    float quadVertices[] = { // vertex attributes for a quad that fills the entire screen in Normalized Device Coordinates.
+        // positions   // texCoords
+        -1.0f,  1.0f,  0.0f, 1.0f,
+        -1.0f, -1.0f,  0.0f, 0.0f,
+        1.0f, -1.0f,  1.0f, 0.0f,
+        
+        -1.0f,  1.0f,  0.0f, 1.0f,
+        1.0f, -1.0f,  1.0f, 0.0f,
+        1.0f,  1.0f,  1.0f, 1.0f
+    };
+    
     unsigned int transparentTexture = loadTexture("/Users/hfy/Svn/openGLTest/openGLTest/blending_transparent_window.png");
     // transparent window locations
     // --------------------------------
@@ -108,9 +106,59 @@ int main()
         glm::vec3(-0.3f, 0.0f, -2.3f),
         glm::vec3( 0.5f, 0.0f, -0.6f)
     };
+
+    // render loop
+    unsigned int transparentVAO, transparentVBO;
+    glGenVertexArrays(1, &transparentVAO);
+    glGenBuffers(1, &transparentVBO);
+    glBindVertexArray(transparentVAO);
+    glBindBuffer(GL_ARRAY_BUFFER, transparentVBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(transparentVertices), transparentVertices, GL_STATIC_DRAW);
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(1);
+    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
+    glBindVertexArray(0);
+    unsigned int quadVAO, quadVBO;
+    glGenVertexArrays(1, &quadVAO);
+    glGenBuffers(1, &quadVBO);
+    glBindVertexArray(quadVAO);
+    glBindBuffer(GL_ARRAY_BUFFER, quadVBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(quadVertices), &quadVertices, GL_STATIC_DRAW);
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(1);
+    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (void*)(2 * sizeof(float)));
     shader.use();
     shader.setInt("texture1", 0);
-    // render loop
+    screenShader.use();
+    screenShader.setInt("screenTexture", 0);
+
+    
+
+    
+    unsigned int framerbuffer;
+    glGenFramebuffers(1,&framerbuffer);
+    glBindFramebuffer(GL_FRAMEBUFFER,framerbuffer);
+    unsigned int texBuffer;
+    glGenTextures(1,&texBuffer);
+    glBindTexture(GL_TEXTURE_2D,texBuffer);
+    glTexImage2D(GL_TEXTURE_2D,0,GL_RGB,SCR_WIDTH,SCR_HEIGHT,0,GL_RGB,GL_UNSIGNED_BYTE,NULL);
+    glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MIN_FILTER,GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MAG_FILTER,GL_LINEAR);
+    glBindTexture(GL_TEXTURE_2D,0);
+    glFramebufferTexture2D(GL_FRAMEBUFFER,GL_COLOR_ATTACHMENT0,GL_TEXTURE_2D,texBuffer,0);
+    
+    unsigned int rbo;
+    glGenRenderbuffers(1, &rbo);
+    glBindRenderbuffer(GL_RENDERBUFFER, rbo);
+    glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH24_STENCIL8, SCR_WIDTH, SCR_HEIGHT); // use a single renderbuffer object for both a depth AND stencil buffer.
+    glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_STENCIL_ATTACHMENT, GL_RENDERBUFFER, rbo); // now actually attach it
+    // now that we actually created the framebuffer and added all attachments we want to check if it is actually complete now
+    if (glCheckFramebufferStatus(GL_FRAMEBUFFER) != GL_FRAMEBUFFER_COMPLETE)
+        cout << "ERROR::FRAMEBUFFER:: Framebuffer is not complete!" << endl;
+    glBindFramebuffer(GL_FRAMEBUFFER, 0);
+
     // -----------
     while (!glfwWindowShouldClose(window))
     {
@@ -130,6 +178,7 @@ int main()
         }
         // render
         // ------
+        glBindFramebuffer(GL_FRAMEBUFFER, framerbuffer);
         glClearColor(0.4f, 0.4f, 0.5f, 1.0f);
         glEnable(GL_DEPTH_TEST);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT );
@@ -155,6 +204,17 @@ int main()
             shader.setMat4("model", model);
             glDrawArrays(GL_TRIANGLES, 0, 6);
         }
+        glBindVertexArray(0);
+        glBindFramebuffer(GL_FRAMEBUFFER, 0);
+        glDisable(GL_DEPTH_TEST); // disable depth test so screen-space quad isn't discarded due to depth test.
+        // clear all relevant buffers
+        glClearColor(1.0f, 1.0f, 1.0f, 1.0f); // set clear color to white (not really necessery actually, since we won't be able to see behind the quad anyways)
+        glClear(GL_COLOR_BUFFER_BIT);
+        screenShader.use();
+        glBindVertexArray(quadVAO);
+        glBindTexture(GL_TEXTURE_2D, texBuffer);    // use the color attachment texture as the texture of the quad plane
+        glDrawArrays(GL_TRIANGLES, 0, 6);
+
         // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
         // -------------------------------------------------------------------------------
         glfwSwapBuffers(window);
